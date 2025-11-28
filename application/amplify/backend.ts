13a6157840f1f675ce00bb0940252bf3347dd564
kagemusha2 <bruno.barberi@gmail.com>
2025-11-28 15:39:45 +0100

    chore: add environment tags and comments to backend

--- application/amplify/backend.ts
+++ application/amplify/backend.ts
@@ -13,3 +13,9 @@ const backend = defineBackend({
 // Ajouter des tags pour identifier les ressources
 backend.stack.tags.setTag('Application', 'TMA');
 backend.stack.tags.setTag('Project', 'Task Manager Application');
+
+// Get the environment name from the stack name (e.g., "main", "develop")
+const env = backend.stack.stackName.split('-').pop() || 'dev';
+
+// Note: Table names are managed by Amplify and include app-id + branch automatically
+// To see your tables: aws dynamodb list-tables --region eu-west-3
